6b2e4456a0e267fdf5e674f5400e4127924220b8
phobo99 <phobotkcb@gmail.com>
2021-10-10 21:05:45 +0700

    git ignore

--- class/app.ts
+++ class/app.ts
@@ -1,19 +1,33 @@
-class CongViec {
-    id: number;
-    ten: string;
-    trangthai: string;
+enum state {
+    Create = 10,
+    Processing,
+    Finish
+}
 
-    constructor(id: number,ten: string,trangthai: string) {
+class CongViec {
+    id: number = 1;
+    ten: string = "Pho";
+    trangthai: state;
+
+    constructor(id: number, ten: string, trangthai: state) {
         this.id = id;
         this.ten = ten;
         this.trangthai = trangthai;
     }
 
-    thongTin(){
+    thongTin() {
         return `${this.id} - ten: ${this.ten} - trang thai: ${this.trangthai}`
+    }
+
+    // ta có thể sử dụng thuộc tính/phương thức của class mà không cần tạo ra một đối tượng mới
+    static testStatic() {
+        console.log("thu xem co su dung duoc ham nay ma khong can phai tao mot instance ko");
     }
 }
 
-let cv1 = new CongViec(1,"Ban hang","Online")
+let cv1 = new CongViec(1, "Ban hang", state.Create)
 
 console.log(cv1)
+console.log(cv1.thongTin());
+console.log(CongViec.testStatic());
+
